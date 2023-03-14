ed3a6255560269a44794005e1f3e9a0ef886cf0a
brewchetta <chett.tiller@gmail.com>
2023-03-14 09:29:36 -0400

    init for state and events

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import StateCard from './StateCard'
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>State and Events</h1>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/StateCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function StateCard(props) {
+  return (
+    <div className="state-card">
+      I am a state card
+    </div>
+  )
+}
+
+export default StateCard
