b2b4bc081ae20f3cbab398e2372ea0cb305b4100
Yael Harosh <yaelharosh01@gmail.com>
2023-03-14 12:02:33 -0400

    useState practice

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import StateCard from './StateCard'
+import { stateNames, stateFlags } from '../data/states';
 
 function App() {
+  const stateList = stateNames.map(state => <StateCard key={ state } state={ state } />);
+  // console.log(stateList);
   return (
     <div className="App">
 
       <h1>State and Events</h1>
 
+      { stateList }
+      
     </div>
   );
 }
--- src/components/StateCard.js
+++ src/components/StateCard.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function StateCard(props) {
+function StateCard({ state }) {
+
+  const [likes, setLikes] = useState(0);
+  const [color, setColor] = useState('lightblue');
+
+  // console.log(like, setLikes)
+  // console.log(useState)
+
+  function handleClick() {
+    // setLikes(likes + 1)
+    // do this way if the value is dependant on its previous value
+    setLikes(likes => likes + 1)
+  }
+
+  function handleColor() {
+    setColor( color === 'lightblue' ? 'orange' : 'lightblue' )
+  }
+
   return (
     <div className="state-card">
-      I am a state card
+
+       <h2>{ state }</h2>
+
+      <button onClick={ handleClick }>{ likes } Like Me!</button>
+
+      <button onClick={ handleColor } style={{background: color }} >Change Color</button>
+
     </div>
   )
 }
